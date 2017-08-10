1dfc15a539bd13004b4d1b62d3e387d0e67537e4
Yuki-Yama <30711183+Yuki-Yama@users.noreply.github.com>
2017-08-10 11:19:19 +0900

    Pushing code to GitHub

---
+++ LongRIDE/ElevationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ConditionViewController.swift
+//  LongRIDE
+//
+//  Created by Yuki on 2017/08/07.
+//  Copyright © 2017年 Yuki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class ElevationViewController : UIViewController{
+    
+
+    @IBOutlet weak var mapview: MKMapView!
+
+    
+}
+
+
